c774fa5549f6bf81cd5bd2a390509e12b6282447
PetrTech <55279432+PetrTech@users.noreply.github.com>
2022-08-06 16:29:00 +0200

    Add project files.

---
+++ IsometricSurvival/Main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+#include <SFML/Graphics.hpp>
+#include "Main.hpp"
+
+int main(/*int argc, char* argv[]*/)
+{
+	printf("Starting Isometrica 2022");
+	
+	sf::RenderWindow window(sf::VideoMode(640,480), "Isometrica 2022");
+
+	// Loading assets
+	sf::Texture voxelTestTexture;
+	if (!voxelTestTexture.loadFromFile("image.png"))
+	{
+		printf("VoxelTest.png failed to load");
+	}
+
+	while (window.isOpen()) {
+		sf::Event event;
+		while (window.pollEvent(event)) {
+			if (event.type == sf::Event::Closed) {
+				window.close();
+			}
+		}
+
+		window.clear();
+		window.display();
+	}
+
+	return 0; // exit code 0, everything went as planned and you still have your PC and RAM! (Luckily no memory leaks happened. I hope.)
+}
